ffdd3db21b48e51fd2f00a35d203ee061393afe1
thesahilkamble <sahil402424@gmail.com>
2024-05-26 18:53:18 +0530

    Node JS Video Lectures

--- WPT/NodeJS/Video Lectures (Revision)/filerreadasyndemo.js
+++ WPT/NodeJS/Video Lectures (Revision)/filerreadasyndemo.js
@@ -8,3 +8,16 @@ fs.readFile("text.txt","utf-8", function(err, data) {
 data = fs.readFileSync("./text.txt");
 console.log("Sync read"+data);
 console.log("Program ends here")
+
+fs.stat("text.txt", function(err,status){
+    if(err){
+        console.log("error "+err.code+"----->" + err.message);
+    }
+    else{
+        console.log(status);
+                console.log(status.isFile());
+                console.log(status.isDirectory());
+
+    }
+
+})
