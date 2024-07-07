cf8a605f086147bf782fc1ebe4333e97cc243400
thesahilkamble <sahil402424@gmail.com>
2024-07-07 23:29:17 +0530

    Ms.Net

--- Ms.Net/Day 1/HelloWorld/Day 1/Program.cs
+++ Ms.Net/Day 1/HelloWorld/Day 1/Program.cs
@@ -4,7 +4,34 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            class1 o = new class1();
+
+            // Positional Params
+
+            Console.WriteLine(o.add(2, 5));
+
+            Console.WriteLine(o.add(5));
+
+            // Named Params
+            Console.WriteLine(o.add(a: 10, b: 5));
+
+            Console.WriteLine(o.add(b: 5));
+
+
+
+            //Console.WriteLine("Hello, World!");
+        }
+    }
+
+    public class class1
+    {
+
+        private int a;
+        private int b;
+
+        public int add(int a = 0, int b = 0, int c = 0)
+        {
+            return a + b;
         }
     }
 }
