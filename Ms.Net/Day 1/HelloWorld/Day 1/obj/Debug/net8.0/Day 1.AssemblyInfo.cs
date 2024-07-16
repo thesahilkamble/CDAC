2895d2601f023e9b52d69158c6199531f66224dd
thesahilkamble <sahil402424@gmail.com>
2024-07-16 22:29:41 +0530

    V2

--- Ms.Net/Day 1/HelloWorld/Day 1/obj/Debug/net8.0/Day 1.AssemblyInfo.cs
+++ Ms.Net/Day 1/HelloWorld/Day 1/obj/Debug/net8.0/Day 1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Day 1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6e76ed1300d0e992c932b10264eb274ff9a12bdd")]
 [assembly: System.Reflection.AssemblyProductAttribute("Day 1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Day 1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
